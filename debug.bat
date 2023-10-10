6672663b020277eea3b8557a21724b1e4c02dea8
alexwithstars <rayo080378@gmail.com>
2023-10-09 22:15:31 -0600

    feat (scripts): new scripts to compile c++

---
+++ debug.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set compile=%1
+set input=%2
+set output=%3
+set res=
+
+if not defined compile (
+	echo Not file provided or bad syntax
+	exit /b 1
+)
+if not exist %compile%.cpp (
+	echo file %compile%.cpp do not exist
+	exit /b 1
+)
+set res=%compile%
+if defined input (
+	if not exist %input%.txt (
+		echo file %input%.txt do not exist
+		exit /b 1
+	)
+	set "res=%res% < %input%.txt"
+)
+if defined output (
+	set "res=%res% > %output%.txt"
+)
+
+echo compiling...
+g++ -DDEBUG %compile%.cpp -o %compile%
+if errorlevel 1 (
+	exit /b 1
+)
+echo runing...
+%res%
+del %compile%.exe
